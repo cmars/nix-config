3a00467b0922087707573fae64bc046f79a90e03
Casey Marshall <git@cmars.tech>
2021-06-13 18:29:44 -0500

    Upgrade home-manager to 21.05

--- install-home-manager.bash
+++ install-home-manager.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eux
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-20.09.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz home-manager
 nix-channel --update
 
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
